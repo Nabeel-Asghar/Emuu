3fde981f4ca18ac056fe78e7491f7de645ee1a44
Nabeel A <nabeel.asghar156@gmail.com>
2020-05-12 19:23:12 -0400

    Added fix for validation of login and signup data

--- photospot-backend/functions/util/validators.js
+++ photospot-backend/functions/util/validators.js
@@ -1,15 +1,15 @@
-const isEmpty = string => {
+const isEmpty = (string) => {
   if (string.trim() === "") return true;
   else return false;
 };
 
-const isEmail = email => {
+const isEmail = (email) => {
   const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (email.match(emailRegEx)) return true;
   else return false;
 };
 
-exports.validateSignUpData = data => {
+exports.validateSignUpData = (data) => {
   let errors = {};
 
   if (isEmpty(data.email)) {
@@ -34,17 +34,13 @@ exports.validateSignUpData = data => {
     errors.handle = "Must not be empty";
   }
 
-  if (Object.keys(errors).length > 0) {
-    return res.status(400).json(errors);
-  }
-
   return {
     errors,
-    valid: Object.keys(errors).length === 0 ? true : false
+    valid: Object.keys(errors).length === 0 ? true : false,
   };
 };
 
-exports.validateLoginData = data => {
+exports.validateLoginData = (data) => {
   let errors = {};
 
   if (isEmpty(data.email)) {
@@ -55,12 +51,8 @@ exports.validateLoginData = data => {
     errors.password = "Must not be empty";
   }
 
-  if (Object.keys(errors).length > 0) {
-    return res.status(400).json(errors);
-  }
-
   return {
     errors,
-    valid: Object.keys(errors).length === 0 ? true : false
+    valid: Object.keys(errors).length === 0 ? true : false,
   };
 };
