2d6fc81d0b651cf4129eec077b6d7e7157c6612f
Nabeel A <nabeel.asghar156@gmail.com>
2020-05-26 15:41:10 -0400

    Added a fully functioning signup page

--- photospot-client/src/pages/signup.js
+++ photospot-client/src/pages/signup.js
@@ -1,13 +1,202 @@
 import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import PropTypes from "prop-types";
+import AppIcon from "../images/logo.png";
+
+// Base Instance
+import API from "../api";
+
+// MUI
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+
+const styles = (theme) => ({
+  ...theme.spreadThis,
+});
 
 class signup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      firstName: "",
+      lastName: "",
+      photographer: "",
+      loading: false,
+      errors: {},
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      loading: true,
+    });
+
+    if (this.state.photographer === "true") {
+      this.state.photographer = true;
+    }
+
+    if (this.state.photographer === "false") {
+      this.state.photographer = false;
+    }
+
+    const newUserData = {
+      email: this.state.email,
+      password: this.state.password,
+      confirmPassword: this.state.confirmPassword,
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      photographer: this.state.photographer,
+    };
+
+    API.post("/signup", newUserData)
+      .then((res) => {
+        console.log(res.data);
+        localStorage.setItem("FireBaseIdToken", `Bearer ${res.data.token}`);
+        this.setState({
+          loading: false,
+        });
+        this.props.history.push("/");
+      })
+      .catch((err) => {
+        this.setState({
+          errors: err.response.data,
+          loading: false,
+        });
+      });
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
   render() {
+    const { classes } = this.props;
+    const { loading, errors } = this.state;
     return (
-      <div>
-        <h1>Signup Page</h1>
-      </div>
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <img src={AppIcon} alt="Logo" className={classes.image} />
+          <Typography variant="h2" className={classes.pageTitle}>
+            Signup
+          </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              className={classes.textField}
+              helperText={errors.email}
+              error={errors.email ? true : false}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+              className={classes.textField}
+              helperText={errors.password}
+              error={errors.password ? true : false}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+              label="Confirm Password"
+              className={classes.textField}
+              helperText={errors.confirmPassword}
+              error={errors.confirmPassword ? true : false}
+              value={this.state.confirmPassword}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="firstName"
+              name="firstName"
+              label="First Name"
+              type="text"
+              className={classes.textField}
+              helperText={errors.firstName}
+              error={errors.firstName ? true : false}
+              value={this.state.firstName}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="lastName"
+              name="lastName"
+              label="Last Name"
+              type="text"
+              className={classes.textField}
+              helperText={errors.lastName}
+              error={errors.lastName ? true : false}
+              value={this.state.lastName}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <br />
+            <br />
+            <FormControl component="fieldset">
+              <FormLabel component="legend">Are you a photographer?</FormLabel>
+              <RadioGroup onChange={this.handleChange} name="photographer">
+                <FormControlLabel
+                  value="true"
+                  control={<Radio />}
+                  label="Yes"
+                />
+                <FormControlLabel
+                  value="false"
+                  control={<Radio />}
+                  label="No"
+                />
+              </RadioGroup>
+            </FormControl>
+
+            {errors.general && (
+              <Typography variant="body2" className={classes.customError}>
+                {errors.general}
+              </Typography>
+            )}
+            <br />
+            <br />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.button}
+            >
+              Signup
+            </Button>
+          </form>
+        </Grid>
+        <Grid item sm />
+      </Grid>
     );
   }
 }
 
-export default signup;
+signup.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(signup);
