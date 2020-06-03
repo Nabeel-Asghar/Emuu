2a00bb60e6d47c152b4a6b341ce8c7ae5d4500d5
Nabeel A <nabeel.asghar156@gmail.com>
2020-06-03 15:56:29 -0400

    Added functionality to store current photographer page in redux store and access it to send to components

--- photospot-client/src/pages/specificPhotographer.js
+++ photospot-client/src/pages/specificPhotographer.js
@@ -5,33 +5,74 @@ import Photographer from "../components/photographer";
 
 // Material UI
 import Grid from "@material-ui/core/Grid";
+import withStyles from "@material-ui/core/styles/withStyles";
 
 // Redux
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getPhotographerPage } from "../redux/actions/dataActions";
 
+const styles = (theme) => ({
+  ...theme.spreadThis,
+});
+
 class specificPhotographer extends Component {
-  state = {
-    profile: null,
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      firstName: "",
+      lastName: "",
+      bio: "",
+      profileImage: "",
+      images: [],
+    };
+  }
+
+  assignStates = (key, value) => {
+    this.setState({
+      [key]: value,
+    });
   };
+
+  assignValues(details) {
+    const formatted = [];
+
+    const photoDetails = Object.values(details);
+
+    photoDetails.forEach((task) =>
+      Object.entries(task).forEach(([key, value]) => {
+        this.assignStates(key, value);
+      })
+    );
+  }
 
   componentDidMount() {
     const photographerID = this.props.match.params.photographerID;
     this.props.getPhotographerPage(photographerID);
+    const photoDetails = this.props.photographerDetails;
+    this.assignValues(photoDetails);
   }
 
   render() {
-    const photoDetails = this.props.photographerDetails;
+    // const photoDetails = this.props.photographerDetails;
 
-    let thePhotographerPage = Object.keys(photoDetails).map((key) => (
-      <Photographer key={key} photographer={photoDetails[key]} />
-    ));
+    // console.log(formatted[0]);
+    // console.log(formatted[1]);
+    // console.log(formatted[2]);
+
+    // let thePhotographerPage = Object.keys(photoDetails).map((key) => (
+    //   <Photographer key={key} photographer={photoDetails[key]} />
+    // ));
+
+    // console.log(thePhotographerPage[0]);
+
+    console.log(this.state.email);
 
     return (
       <Grid container spacing={10}>
         <Grid item sm={8} xs={12}>
-          {thePhotographerPage}
+          {/* {thePhotographerPage} */}
         </Grid>
         <Grid item sm={4} xs={12}></Grid>
       </Grid>
@@ -55,4 +96,4 @@ const mapActionsToProps = {
 export default connect(
   mapStateToProps,
   mapActionsToProps
-)(specificPhotographer);
+)(withStyles(styles)(specificPhotographer));
