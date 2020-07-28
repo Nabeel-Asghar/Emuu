274d8184d0400d50d84ff3841295ec836f1a46fc
Nabeel A <nabeel.asghar156@gmail.com>
2020-07-27 23:44:25 -0400

    Flexbox for navbar instead of grids

--- photospot-client/src/components/Navbar.js
+++ photospot-client/src/components/Navbar.js
@@ -13,6 +13,7 @@ import Menu from "@material-ui/core/Menu";
 import { AccountCircle } from "@material-ui/icons";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
 
 // Redux
 import { logoutUser } from "../redux/actions/userActions";
@@ -36,7 +37,7 @@ const Navbar = () => {
   };
 
   const container = {
-    width: "500px",
+    width: "1000px",
     margin: "auto",
   };
 
@@ -52,71 +53,78 @@ const Navbar = () => {
   return (
     <AppBar>
       <Toolbar>
-        <Grid container alignItems="center">
-          <Grid item xs={3} />
-          <Grid item xs={1} style={centerGrid}>
-            <IconButton edge="start" component={Link} to="/">
-              <img src={AppIcon} alt="Logo" style={imageStyle} />
-            </IconButton>
-          </Grid>
-          <Grid item xs={4} />
-          <Grid item xs={1} style={centerGrid}>
-            <IconButton
-              edge="end"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit"
-            >
-              <AccountCircle fontSize="large" />
-            </IconButton>
-          </Grid>
-          <Grid item xs={3} />
-          {authenticated && (
-            <div>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                keepMounted
-                open={open}
-                onClose={handleClose}
+        <div style={{ width: 1000, height: "50px", margin: "auto" }}>
+          <Box display="flex" p={1}>
+            <Box p={1} flexGrow={1}>
+              <IconButton
+                edge="start"
+                style={{ padding: 0, marginTop: "-10px" }}
+                component={Link}
+                to="/"
               >
-                <MenuItem component={Link} to="/profile">
-                  Profile
-                </MenuItem>
+                <img src={AppIcon} alt="Logo" style={imageStyle} />
+              </IconButton>
+            </Box>
 
-                <MenuItem component={Link} to="/yourPhotographyProfile">
-                  Photographer Page
-                </MenuItem>
-
-                <MenuItem onClick={() => dispatch(logoutUser())}>
-                  Logout
-                </MenuItem>
-              </Menu>
-            </div>
-          )}
-
-          {!authenticated && (
-            <div>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                keepMounted
-                open={open}
-                onClose={handleClose}
+            <Box p={1}>
+              <IconButton
+                style={{ padding: 0, marginTop: "-10px" }}
+                edge="end"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
               >
-                <MenuItem component={Link} to="/login">
-                  Login
-                </MenuItem>
+                <AccountCircle fontSize="large" />
+              </IconButton>
+            </Box>
 
-                <MenuItem component={Link} component={Link} to="/signup">
-                  Signup
-                </MenuItem>
-              </Menu>
-            </div>
-          )}
-        </Grid>
+            {authenticated && (
+              <div>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  keepMounted
+                  open={open}
+                  onClose={handleClose}
+                >
+                  <MenuItem component={Link} to="/profile">
+                    Profile
+                  </MenuItem>
+
+                  <MenuItem component={Link} to="/yourPhotographyProfile">
+                    Photographer Page
+                  </MenuItem>
+
+                  <MenuItem onClick={() => dispatch(logoutUser())}>
+                    Logout
+                  </MenuItem>
+                </Menu>
+              </div>
+            )}
+
+            {!authenticated && (
+              <div>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  keepMounted
+                  open={open}
+                  onClose={handleClose}
+                >
+                  <MenuItem component={Link} to="/login">
+                    Login
+                  </MenuItem>
+
+                  <MenuItem component={Link} component={Link} to="/signup">
+                    Signup
+                  </MenuItem>
+                </Menu>
+              </div>
+            )}
+          </Box>
+        </div>
       </Toolbar>
     </AppBar>
   );
