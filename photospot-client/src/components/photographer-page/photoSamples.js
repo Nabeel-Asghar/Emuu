9249d97e4c0fa98a641e811a3e3e894743c4f67f
Nabeel A <nabeel.asghar156@gmail.com>
2020-07-28 17:52:02 -0400

    Photos can now be enlarged and shown in a lightbox

--- photospot-client/src/components/photographer-page/photoSamples.js
+++ photospot-client/src/components/photographer-page/photoSamples.js
@@ -1,19 +1,29 @@
 import React, { Component } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
-import ImageGallery from "react-image-gallery";
-import "react-image-gallery/styles/css/image-gallery.css";
 import Skeleton from "@material-ui/lab/Skeleton";
 import Gallery from "react-photo-gallery";
+import Button from "@material-ui/core/Button";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
 
 const styles = (theme) => ({
   ...theme.spreadThis,
 });
 
 class photoSamples extends Component {
+  constructor() {
+    super();
+    this.state = {
+      photoIndex: 0,
+      isOpen: false,
+    };
+  }
+
   render() {
     const { classes, images, loading } = this.props;
 
     var imageContainer = [];
+    var lightboxImages = [];
 
     for (var i = 0; i < images.length; i++) {
       var img = new Image();
@@ -22,7 +32,7 @@ class photoSamples extends Component {
       let width = Math.ceil(img.width / 100);
       let height = Math.ceil(img.height / 100);
 
-      console.log(width, height);
+      lightboxImages.push(img.src);
 
       imageContainer.push({
         src: img.src,
@@ -30,13 +40,49 @@ class photoSamples extends Component {
         height: height,
       });
     }
-
+    const { photoIndex, isOpen } = this.state;
     return (
       <div>
         {loading ? (
           <Skeleton variant="rect" height={500} width={500} />
         ) : (
-          <Gallery photos={imageContainer} />
+          <div>
+            <Button
+              type="Button"
+              onClick={() => this.setState({ isOpen: true })}
+            >
+              Open Images
+            </Button>
+            {isOpen && (
+              <Lightbox
+                mainSrc={lightboxImages[photoIndex]}
+                nextSrc={
+                  lightboxImages[(photoIndex + 1) % lightboxImages.length]
+                }
+                prevSrc={
+                  lightboxImages[
+                    (photoIndex + lightboxImages.length - 1) %
+                      lightboxImages.length
+                  ]
+                }
+                onCloseRequest={() => this.setState({ isOpen: false })}
+                onMovePrevRequest={() =>
+                  this.setState({
+                    photoIndex:
+                      (photoIndex + lightboxImages.length - 1) %
+                      lightboxImages.length,
+                  })
+                }
+                onMoveNextRequest={() =>
+                  this.setState({
+                    photoIndex: (photoIndex + 1) % lightboxImages.length,
+                  })
+                }
+              />
+            )}
+
+            <Gallery photos={imageContainer} />
+          </div>
         )}
       </div>
     );
