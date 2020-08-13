43de51033edcc3a4c1ac4cde1e733baa47c6c618
Nabeel A <nabeel.asghar156@gmail.com>
2020-08-12 21:01:39 -0400

    Stopped annoying console output

--- photospot-client/src/components/photographer-page/review.js
+++ photospot-client/src/components/photographer-page/review.js
@@ -162,8 +162,7 @@ class review extends Component {
       UI: { loadingReviewAction, newReviewSucess },
     } = this.props;
     const { errors } = this.state;
-    console.log(errors);
-    console.log(errors.title);
+
     return (
       <Collapse in={checked}>
         <div>
