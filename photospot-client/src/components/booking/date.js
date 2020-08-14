1bd0919be92fd7e9f28581cafe36a7893842c88e
Nabeel A <nabeel.asghar156@gmail.com>
2020-08-14 13:22:47 -0400

    Fixed styling on time and date

--- photospot-client/src/components/booking/date.js
+++ photospot-client/src/components/booking/date.js
@@ -3,6 +3,19 @@ import React, { Component } from "react";
 import { DatePicker } from "@material-ui/pickers";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import MomentUtils from "@date-io/moment";
+import { createMuiTheme } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
+
+const defaultMaterialTheme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#65edbb",
+      main: "#23ba8b",
+      dark: "#00895e",
+      contrastText: "#ffffff",
+    },
+  },
+});
 
 class date extends Component {
   render() {
@@ -11,15 +24,18 @@ class date extends Component {
     return (
       <div>
         <MuiPickersUtilsProvider utils={MomentUtils}>
-          <DatePicker
-            autoOk
-            orientation="landscape"
-            variant="static"
-            openTo="date"
-            value={theDate}
-            onChange={this.props.parentCallback}
-            format="MM/dd/yyyy"
-          />
+          <ThemeProvider theme={defaultMaterialTheme}>
+            <DatePicker
+              disablePast
+              autoOk
+              orientation="landscape"
+              variant="static"
+              openTo="date"
+              value={theDate}
+              onChange={this.props.parentCallback}
+              format="MM/dd/yyyy"
+            />
+          </ThemeProvider>
         </MuiPickersUtilsProvider>
       </div>
     );
