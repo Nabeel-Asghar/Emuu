a05d7e40eea841ee9254dc32c90e2dc624e86c31
Nabeel A <nabeel.asghar156@gmail.com>
2020-09-16 12:05:02 -0400

    Send emails for refund

--- photospot-backend/functions/handlers/email.js
+++ photospot-backend/functions/handlers/email.js
@@ -4,11 +4,53 @@ const SendGridKey = process.env.SENDGRID_API_KEY;
 sgMail.setApiKey(SendGridKey);
 
 const emailOrderDetails = (orderDetails) => {
-  emailToCustomer(orderDetails);
-  emailToPhotographer(orderDetails);
+  emailOrderToCustomer(orderDetails);
+  emailOrderToPhotographer(orderDetails);
 };
 
-function emailToCustomer(orderDetails) {
+const emailRefund = (orderDetails) => {
+  emailRefundToCustomer(orderDetails);
+  emailRefundToPhotographer(orderDetails);
+};
+
+function emailRefundToCustomer(orderDetails) {
+  const msg = {
+    to: orderDetails.consumerEmail,
+    from: "PhotoSpot@photospot.site",
+    templateId: "d-3b74ac26b55f416fa501a35b5f811279",
+    dynamic_template_data: {
+      consumerFirstName: orderDetails.consumerFirstName,
+      photographerFirstName: orderDetails.photographerFirstName,
+      photographerLastName: orderDetails.photographerLastName,
+      shootDate: orderDetails.shootDate,
+      shootTime: orderDetails.shootTime,
+      amount: orderDetails.amount,
+    },
+  };
+  sgMail.send(msg);
+  console.log("Refund sent to customer");
+}
+
+function emailRefundToPhotographer(orderDetails) {
+  const msg = {
+    to: orderDetails.photographerEmail,
+    from: "PhotoSpot@photospot.site",
+    templateId: "d-d38e871ea3ff4f399de583f204b57597",
+    dynamic_template_data: {
+      photographerFirstName: orderDetails.photographerFirstName,
+      consumerFirstName: orderDetails.consumerFirstName,
+      consumerLastName: orderDetails.consumerLastName,
+      consumerEmail: orderDetails.consumerEmail,
+      shootDate: orderDetails.shootDate,
+      shootTime: orderDetails.shootTime,
+      amount: orderDetails.amount,
+    },
+  };
+  sgMail.send(msg);
+  console.log("Refund sent to photographer");
+}
+
+function emailOrderToCustomer(orderDetails) {
   let url =
     "http://localhost:3000/photographers/" + orderDetails.photographerID;
 
@@ -30,7 +72,7 @@ function emailToCustomer(orderDetails) {
   console.log("Email sent to customer!");
 }
 
-function emailToPhotographer(orderDetails) {
+function emailOrderToPhotographer(orderDetails) {
   const msg = {
     to: orderDetails.photographerEmail,
     from: "PhotoSpot@photospot.site",
@@ -42,6 +84,7 @@ function emailToPhotographer(orderDetails) {
       consumerEmail: orderDetails.consumerEmail,
       shootDate: orderDetails.shootDate,
       shootTime: orderDetails.shootTime,
+      amount: orderDetails.amount,
     },
   };
   sgMail.send(msg);
@@ -49,3 +92,4 @@ function emailToPhotographer(orderDetails) {
 }
 
 exports.emailOrderDetails = emailOrderDetails;
+exports.emailRefunds = emailRefund;
