cc938b65a10b14ea887f00e2598b8e8fae961e05
Adeel Asghar <adeelasghar1001@gmail.com>
2021-05-22 19:01:53 -0400

    Added environment

--- photospot-client/src/util/firestore.js
+++ photospot-client/src/util/firestore.js
@@ -11,6 +11,16 @@ const config = {
   measurementId: "G-FSEHN9YLHE",
 };
 
-firebase.initializeApp(config);
+const firebaseConfig = {
+  apiKey: "AIzaSyAY_hC7Jjg3N8hsNZzp-8TzdedxpqcHOds",
+  authDomain: "photospot-dev-b174a.firebaseapp.com",
+  projectId: "photospot-dev-b174a",
+  storageBucket: "photospot-dev-b174a.appspot.com",
+  messagingSenderId: "458864535144",
+  appId: "1:458864535144:web:0d71202acbde79076c3ddd",
+  measurementId: "G-1VFRQT634Y",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export { firebase };
