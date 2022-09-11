fa5dcbd81052e9400af536781bde49782ed6e811
Moamin1 <gw3810@wayne.edu>
2022-09-11 13:41:43 -0400

    Added firebase integration

--- emuu-server/users/register.go
+++ emuu-server/users/register.go
@@ -5,11 +5,13 @@ import (
 )
 
 func RegisterUser(c *gin.Context) {
-	//email := c.PostForm("email")
-	//username := c.PostForm("username")
-	//password := c.PostForm("password")
-	//fName := c.PostForm("firstName")
-	//lName := c.PostForm("lastName")
+
+
+// 	email := c.PostForm("email")
+// 	username := c.PostForm("username")
+// 	password := c.PostForm("password")
+//fName := c.PostForm("firstName")
+//lName := c.PostForm("lastName")
 
 	c.JSON(200, gin.H{"message": "User has registered successfully!"})
 }
