67cade8ba395f718845b07df3681f6c749c5e73b
Tanvir-Rahman123 <gw4123@wayne.edu>
2022-09-11 15:11:15 -0400

    Initial setup of registering user

--- emuu-server/users/register.go
+++ emuu-server/users/register.go
@@ -10,6 +10,35 @@ func RegisterUser(c *gin.Context) {
 	//password := c.PostForm("password")
 	//fName := c.PostForm("firstName")
 	//lName := c.PostForm("lastName")
+// Add validation to registration
+	valid := helpers.Validation(
+		[]interfaces.Validation{
+			{Value: username, Valid: "username"},
+			{Value: email, Valid: "email"},
+			{Value: pass, Valid: "password"},
+		})
+	if valid {
+		// Create registration logic
+		// Connect DB
+		db := helpers.ConnectDB()
+		generatedPassword := helpers.HashAndSalt([]byte(pass))
+		user := &interfaces.User{Username: username, Email: email, Password: generatedPassword}
+		db.Create(&user)
 
+		account := &interfaces.Account{Type: "Registered Account", Name: string(username + "'s" + " account"), Balance: 0, UserID: user.ID}
+		db.Create(&account)
+
+		defer db.Close()
+		accounts := []interfaces.ResponseAccount{}
+		respAccount := interfaces.ResponseAccount{ID: account.ID, Name: account.Name, Email: account.Email}
+		accounts = append(accounts, respAccount)
+		var response = prepareResponse(user, accounts)
+
+		return response
+	} else {
+		return map[string]interface{}{"message": "not valid values"}
+	}
+
+}
 	c.JSON(200, gin.H{"message": "User has registered successfully!"})
 }
