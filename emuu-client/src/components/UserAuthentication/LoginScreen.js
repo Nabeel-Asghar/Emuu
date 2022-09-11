e293fea4d0bdf2d3b213b240e6672e5c839cdd59
Rabby Ahmed <rabbyahmed123098@gmail.com>
2022-09-11 14:35:37 -0400

    Created a Navigation Bar (Login page, Register page, Home Page), imported bootstrap and router-dom

---
+++ emuu-client/src/components/UserAuthentication/LoginScreen.js
@@ -0,0 +1,30 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#484848",
+      main: "#212121",
+      dark: "#000000",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#6bffff",
+      main: "#0be9d0",
+      dark: "#00b69f",
+      contrastText: "#000",
+    },
+  },
+});
+function Login() {
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="Login">
+      <h1>             </h1>
+      <h1>Login</h1>
+
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default Login;
