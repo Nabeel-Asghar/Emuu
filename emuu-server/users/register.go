08e51f115dc17b4ea6c69bdf6093488a421e224e
Tanvir-Rahman123 <gw4123@wayne.edu>
2022-09-13 13:01:52 -0400

    Connecting registration to local host

--- emuu-server/users/register.go
+++ emuu-server/users/register.go
@@ -11,10 +11,12 @@ func RegisterUser(c *gin.Context) {
     mux := http.NewServeMux()
     mux.HandleFunc("/Register", func(w http.ResponseWriter, r *http.Request)
     {
+    //Parse form data from registration page
     r.ParseForm()
 
 
     }
+    //
 	email := c.PostForm("email")
 	username := c.PostForm("username")
 	password := c.PostForm("password")
@@ -22,6 +24,7 @@ func RegisterUser(c *gin.Context) {
 	lName := c.PostForm("lastName")
 
 
+   //Pass form value data into a json file
 	c.JSON(200, gin.H{"message": "User has registered successfully!"})
 	c.JSON(200, gin.H{"message": r.FormValue("email")})
 	c.JSON(200, gin.H{"message": r.FormValue("username")})
@@ -29,6 +32,6 @@ func RegisterUser(c *gin.Context) {
 	c.JSON(200, gin.H{"message": r.FormValue("firstName")})
 	c.JSON(200, gin.H{"message": r.FormValue("lastName")})
 
-
+http.ListenAndServe(":8080", mux)
 
 }
