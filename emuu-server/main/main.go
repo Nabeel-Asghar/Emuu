4fa6db7d4ec475ccbbfabda831cbc169d29da81f
Tanvir-Rahman123 <gw4123@wayne.edu>
2022-09-17 23:13:40 -0400

    Fixed registration function, it now pulls data from front end and is stored in backend, need to push data into firebase

--- emuu-server/main/main.go
+++ emuu-server/main/main.go
@@ -4,24 +4,21 @@ import (
 
     "log"
 
-	"emuu-server/main/firebase"
-	//status "emuu-server/main/status"
- 	register "emuu-server/main/users"
-	//"github.com/gin-gonic/gin"
+   "emuu-server/main/firebase"
+   //status "emuu-server/main/status"
+   register "emuu-server/main/users"
+   //"github.com/gin-gonic/gin"
 )
 
 
 
 
 func main() {
-	err := firebase.Init()
-	if err != nil {
-		log.Fatalf("error initializing firebase %v", err)
-	}
-	log.Println("Firebase Connected")
-	//register.RegisterUser()
+   err := firebase.Init()
+   if err != nil {
+      log.Fatalf("error initializing firebase %v", err)
+   }
+   log.Println("Firebase Connected")
+   register.RegisterUser()
 
 }
-
-
-
