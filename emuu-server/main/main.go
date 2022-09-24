ee29601ffccc75f0fe88e8fc8616a28cfad00519
Moamin1 <gw3810@wayne.edu>
2022-09-23 23:27:51 -0400

    Added Gin Gonic Middleware and gin handlers for user verification, retreived token data from firebase and tested api endpoints to ensure that the token matched up

--- emuu-server/main/main.go
+++ emuu-server/main/main.go
@@ -1,44 +1,39 @@
 package main
 
 import (
+	firebaseSer "emuu-server/main/firebase"
+	register "emuu-server/main/users"
+	"fmt"
+	"log"
 
-    //"log"
-"fmt"
-
-   //status "emuu-server/main/status"
-   register "emuu-server/main/users"
-   //"github.com/gin-gonic/gin"
-   //auth "emuu-server/main/firebase"
+	"github.com/gin-gonic/gin"
 )
 
-
-
-
-
-
-
 func main() {
-    fmt.Println("Host 8080 Listening...")
-   register.RegisterUser()
+	fmt.Println("Host 8080 Listening...")
+	// initialize new gin engine (for server)
+	r := gin.Default() //default router
+	// configure firebase
+	firebaseAuth := firebaseSer.InitAuth()//calling firebase auth client
 
+	// set db & firebase auth to gin context with a middleware to all incoming request
+	r.Use(func(c *gin.Context) { //register that function library
+		c.Set("firebaseAuth", firebaseAuth)
+	})
 
+	auth := r.Group("auth") //group is in the gin gonic framework,if you want to create login forgot password, you can create it
+	{
+		auth.POST("/register", register.CreateUser)
+		auth.POST("/login", register.CreateUser)
+	}
 
+	api := r.Group("api").Use(firebaseSer.AuthJWT)//create a new router with the middleware authJWT
+	{//you should supply the jwt token from firebase
+		api.POST("/", func(ctx *gin.Context) {
+			log.Println("Hello")
+		})
+	}
 
-
+	log.Println("Server started at 8080")
+	r.Run(":8080")
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
