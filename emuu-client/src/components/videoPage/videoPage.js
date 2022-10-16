27aa4dcc1357cbb66b03352a02ed436076f5e13e
Tanvir-Rahman123 <gw4123@wayne.edu>
2022-10-16 03:57:10 -0400

    Video page now pulls information such as description, likes, views, game tag. Date does not work as we stored it as timestamp, will need to store as string. Added instagram like button. Will need to add comments section and work on front end styling.

--- emuu-client/src/components/videoPage/videoPage.js
+++ emuu-client/src/components/videoPage/videoPage.js
@@ -1,4 +1,9 @@
 import {useState , useEffect} from 'react';
+import React from 'react';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Favorite from '@material-ui/icons/Favorite';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
 
 function Video ({video , setVideo}){
 useEffect(()=>{
@@ -16,8 +21,19 @@ window.location.pathname="/" //redirects to home
 
 return(
 <div className="videoPage">
-<video src={video.VideoUrl} controls></video>
+<video controls height = "500" src={video.VideoUrl} ></video>
 <h2>{video.VideoTitle}</h2>
+<p>{video.VideoDescription} | {video.Likes} Likes | {video.Views} Views |
+ <FormControlLabel
+        control={<Checkbox icon={<FavoriteBorder />}
+                  checkedIcon={<Favorite />}
+          name="Like" />}
+        label="Like"
+      />
+</p>
+<p>Posted By: {video.Username} on </p>
+<p>Game Tag: {video.GameTag}</p>
+<h3> Comments </h3>
 </div>
 )
 }
