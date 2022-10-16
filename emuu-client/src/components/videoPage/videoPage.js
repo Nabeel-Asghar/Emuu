c26fe23f70a3f9588022e53c4eb7971b514e96ee
Tanvir-Rahman123 <gw4123@wayne.edu>
2022-10-16 03:35:02 -0400

    Video page now reloads with video, used local storage for the meantime. Will see if we can do it off of url.

--- emuu-client/src/components/videoPage/videoPage.js
+++ emuu-client/src/components/videoPage/videoPage.js
@@ -1,7 +1,18 @@
 import {useState , useEffect} from 'react';
 
 function Video ({video , setVideo}){
+useEffect(()=>{
+if(video)localStorage.setItem('video',JSON.stringify(video));
+else{
+if(localStorage.getItem('video'))setVideo(JSON.parse(localStorage.getItem('video')));
+else{
+window.location.pathname="/" //redirects to home
+}
+}
 
+
+
+}, [])
 
 return(
 <div className="videoPage">
