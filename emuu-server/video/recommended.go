25f9e0a2003cb86698557c14f5f27099ba7433a7
Tanvir-Rahman123 <gw4123@wayne.edu>
2022-11-26 22:09:04 -0500

    adding algolia to recommended.go

--- emuu-server/video/recommended.go
+++ emuu-server/video/recommended.go
@@ -3,21 +3,23 @@ package video
 import (
 	"cloud.google.com/go/firestore"
 	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 	"log"
 	"net/http"
-	"time"
 	"reflect"
-
+	"time"
 )
 
 type GamesTag struct {
-	GameTag string `json:"gameTag"`
+	GameTag        string `json:"gameTag"`
+	RecommendTitle string `json:"title"`
 }
 
 var Games string
+var RecTitle string
 
 type Vid struct {
 	Username         string              `firestore:"Username"`
@@ -32,8 +34,7 @@ type Vid struct {
 	VideoDescription string              `firestore:"VideoDescription"`
 	UsersThatLiked   []string            `firestore:"usersThatLiked"`
 	Comments         []map[string]string `firestore:"Comments,omitempty"`
-	ProfilePic string
-
+	ProfilePic       string
 }
 
 func SetGameTag(c *gin.Context) {
@@ -42,11 +43,12 @@ func SetGameTag(c *gin.Context) {
 	c.ShouldBindJSON(&res)
 	c.JSON(http.StatusOK, gin.H{"message": res})
 	Games = res.GameTag
+	RecTitle = res.RecommendTitle
 
 }
 
 func SetRecommended(c *gin.Context) {
-
+	var ID string
 	recommendArr := []Vid{}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15) //setting context with timeout 15
 	defer cancel()                                                           //after 15 seconds, if the function is not executed it will cancel and throw an error
@@ -69,6 +71,9 @@ func SetRecommended(c *gin.Context) {
 		var vid = Vid{}
 
 		doc.DataTo(&vid)
+		if doc.Data().Title == RecTitle {
+			ID = doc.Ref.ID
+		}
 		iter := client.Collection("Users").Where("Username", "==", vid.Username).Documents(ctx)
 		for {
 			doc, err := iter.Next()
@@ -85,6 +90,9 @@ func SetRecommended(c *gin.Context) {
 		}
 		recommendArr = append(recommendArr, vid)
 	}
+	options := recommend.NewRelatedProductsOptions("Videos", ID, 4, nil, nil, nil)
+	res, err = client.GetRelatedProducts([]recommend.RelatedProductsOptions{options})
+	fmt.Println(res)
 
 	response := struct {
 		RecommendedVideos []Vid
