fef105919fc651ad5073047bd5e17c19e4de935f
Rabby Ahmed <rabbyahmed123098@gmail.com>
2022-11-27 21:50:20 -0500

    temp comment code to run server

--- emuu-server/video/recommended.go
+++ emuu-server/video/recommended.go
@@ -3,7 +3,7 @@ package video
 import (
 	"cloud.google.com/go/firestore"
 	"context"
-	"fmt"
+// 	"fmt"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
@@ -48,7 +48,7 @@ func SetGameTag(c *gin.Context) {
 }
 
 func SetRecommended(c *gin.Context) {
-	var ID string
+// 	var ID string
 	recommendArr := []Vid{}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15) //setting context with timeout 15
 	defer cancel()                                                           //after 15 seconds, if the function is not executed it will cancel and throw an error
@@ -71,9 +71,9 @@ func SetRecommended(c *gin.Context) {
 		var vid = Vid{}
 
 		doc.DataTo(&vid)
-		if doc.Data().Title == RecTitle {
-			ID = doc.Ref.ID
-		}
+// 		if doc.Data().Title == RecTitle {
+// 			ID = doc.Ref.ID
+// 		}
 		iter := client.Collection("Users").Where("Username", "==", vid.Username).Documents(ctx)
 		for {
 			doc, err := iter.Next()
@@ -90,9 +90,9 @@ func SetRecommended(c *gin.Context) {
 		}
 		recommendArr = append(recommendArr, vid)
 	}
-	options := recommend.NewRelatedProductsOptions("Videos", ID, 4, nil, nil, nil)
-	res, err = client.GetRelatedProducts([]recommend.RelatedProductsOptions{options})
-	fmt.Println(res)
+// 	options := recommend.NewRelatedProductsOptions("Videos", ID, 4, nil, nil, nil)
+// 	res, err = client.GetRelatedProducts([]recommend.RelatedProductsOptions{options})
+// 	fmt.Println(res)
 
 	response := struct {
 		RecommendedVideos []Vid
