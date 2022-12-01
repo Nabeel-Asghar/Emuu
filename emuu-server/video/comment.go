4453f72462110e5b84b450be21435108da1c6b74
Tanvir-Rahman123 <gw4123@wayne.edu>
2022-12-01 01:58:50 -0500

    Commented code for comment, subscriber, subscription, and upload

--- emuu-server/video/comment.go
+++ emuu-server/video/comment.go
@@ -3,7 +3,6 @@ package video
 import (
 	"cloud.google.com/go/firestore"
 	"context"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
@@ -12,23 +11,30 @@ import (
 	"time"
 )
 
+// create global variable for video url
 var videoUrl string
 
+// create struct for comment information sent from frontend
 type VideoInfo struct {
 	UserName string `json:"postedBy"`
 	VideoUrl string `json:"videoUrl"`
 	Comment  string `json:"text"`
 }
+
+// create comment struct to reflect document field of Comments under the Videos collection
 type Comment struct {
 	Comments []map[string]string `firestore:"Comments"`
 }
 
+// function to set comments
 func SetComment(c *gin.Context) {
+	//bind to json and get the comment info sent from frontend
 	var input VideoInfo
 	if err := c.ShouldBindJSON(&input); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()}) //writter with particular error
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()}) //written with particular error
 		return
 	}
+	//set video url
 	videoUrl = input.VideoUrl
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15) //setting context with timeout 15
 	defer cancel()                                                           //after 15 seconds, if the function is not executed it will cancel and throw an error
@@ -39,10 +45,13 @@ func SetComment(c *gin.Context) {
 		log.Fatalf("firestore client creation error:%s", err)
 	}
 	defer client.Close()
+	//create comments array
 	commentsArr := Comment{}
+	//get current time
 	dt := time.Now()
+	//create comment element based off of information sent from frontend and current date
 	comment := map[string]string{"postedBy": input.UserName, "text": input.Comment, "date": dt.Format("01-02-2006")}
-
+	//iterate through Videos collection to find video that comment was posted on
 	iter := client.Collection("Videos").Where("VideoUrl", "==", videoUrl).Documents(ctx)
 	for {
 		doc, err := iter.Next()
@@ -52,11 +61,11 @@ func SetComment(c *gin.Context) {
 		if err != nil {
 			return
 		}
-
+		//get data of comments array from Firestore
 		doc.DataTo(&commentsArr)
-
+		//add new comment to array
 		commentsArr.Comments = append(commentsArr.Comments, comment)
-
+		//update comments to have the new comment in Firestore
 		dc := client.Collection("Videos").Doc(doc.Ref.ID)
 		_, err = dc.Update(ctx, []firestore.Update{
 			{Path: "Comments", Value: commentsArr.Comments},
@@ -65,8 +74,8 @@ func SetComment(c *gin.Context) {
 
 }
 
+// function to get comments
 func GetComment(c *gin.Context) {
-	fmt.Println("Hello" + videoUrl)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15) //setting context with timeout 15
 	defer cancel()                                                           //after 15 seconds, if the function is not executed it will cancel and throw an error
 	// Firestore initialized
@@ -76,7 +85,9 @@ func GetComment(c *gin.Context) {
 		log.Fatalf("firestore client creation error:%s", err)
 	}
 	defer client.Close()
+	//create comments array
 	commentsArr := Comment{}
+	//find video in Firestore
 	iter := client.Collection("Videos").Where("VideoUrl", "==", videoUrl).Documents(ctx)
 	for {
 		doc, err := iter.Next()
@@ -86,9 +97,10 @@ func GetComment(c *gin.Context) {
 		if err != nil {
 			return
 		}
+		//get comments array data from Firestore and add to comments array
 		doc.DataTo(&commentsArr)
 
 	}
-
+	//send response to frontend
 	c.JSON(http.StatusOK, gin.H{"message": commentsArr})
 }
