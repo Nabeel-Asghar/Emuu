8e4b9ddbb70eea985ae343f416b7ad973c8857ee
Tanvir-Rahman123 <gw4123@wayne.edu>
2022-12-01 01:39:56 -0500

    Commented code for navbar, profile pic, register, settings, subscribe button

--- emuu-server/users/profilePic.go
+++ emuu-server/users/profilePic.go
@@ -11,12 +11,15 @@ import (
 	"time"
 )
 
+// Create struct to reflect values of JSON elements sent from axios request on frontend
 type ProfileInfo struct {
 	UserName string `json:"displayName"`
 	Image    string `json:"profileImageUrl"`
 }
 
+// function to update profile picture
 func UpdateProfile(c *gin.Context) {
+	//create variable input and bind JSON to it to retreive username and image url
 	var input ProfileInfo
 	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()}) //writter with particular error
@@ -32,6 +35,7 @@ func UpdateProfile(c *gin.Context) {
 		log.Fatalf("firestore client creation error:%s", err)
 	}
 	defer client.Close()
+	//update profile picture url in Users collection in the user's document
 	bannerUpdate, err := client.Collection("Users").Doc(input.UserName).Set(ctx, map[string]interface{}{
 		"ProfilePictureUrl": input.Image,
 	}, firestore.MergeAll)
--- emuu-server/users/subscribeButton.go
+++ emuu-server/users/subscribeButton.go
@@ -15,19 +15,23 @@ import (
 	"time"
 )
 
+// create global variables for subscriber and creator
 var subName string
 var creatorName string
 
+// create user and creator struct to reflect JSON elements sent from frontend
 type UserAndCreator struct {
 	UserName   string `json:"displayName"`
 	Creator    string `json:"creatorName"`
 	SubBoolean bool   `json:"SubBoolean"`
 }
 
+// create subscriber struct to reflect firestore value of subscriber list
 type Subscriber struct {
 	UsersThatSubscribed []string `firestore:"SubscriberList"`
 }
 
+// set the user and creator global variables by binding to JSON
 func SetUserAndCreator(c *gin.Context) {
 	var res UserAndCreator
 	c.ShouldBindJSON(&res)
@@ -37,6 +41,7 @@ func SetUserAndCreator(c *gin.Context) {
 
 }
 
+// function to check if user is subscribed
 func CheckSub(c *gin.Context) {
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15) //setting context with timeout 15
@@ -48,7 +53,7 @@ func CheckSub(c *gin.Context) {
 		log.Fatalf("firestore client creation error:%s", err)
 	}
 	defer client.Close()
-
+	//create subscriber array
 	var subArr Subscriber
 	iter := client.Collection("Users").Where("Username", "==", creatorName).Documents(ctx)
 	for {
@@ -59,9 +64,10 @@ func CheckSub(c *gin.Context) {
 		if err != nil {
 			return
 		}
-
+		//iterate through Firestore data to find subscribers array of the creator
 		doc.DataTo(&subArr)
 		var checkedStatus bool = false
+		//check if user is contained within the array, if they are, send true response
 		for i := 0; i < len(subArr.UsersThatSubscribed); i++ {
 			if subArr.UsersThatSubscribed[i] == subName {
 				checkedStatus = true
@@ -69,7 +75,7 @@ func CheckSub(c *gin.Context) {
 			}
 
 		}
-
+		//Send the response to frontend of whether user is subscribed
 		response := struct {
 			CheckedSubValue bool
 		}{
@@ -82,7 +88,9 @@ func CheckSub(c *gin.Context) {
 
 }
 
+// function to subscribe to user
 func SetSubscribe(c *gin.Context) {
+	//bind to json and get user and creator
 	var input UserAndCreator
 	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()}) //writter with particular error
@@ -98,10 +106,12 @@ func SetSubscribe(c *gin.Context) {
 		log.Fatalf("firestore client creation error:%s", err)
 	}
 	defer client.Close()
-
+	//if user subscribes (true)
 	if input.SubBoolean == true {
+		//create subscriber array to retrieve current array and new array for updated user
 		var subArr Subscriber
 		var newsubArr []string
+		//iterate through Firestore to find creator document
 		iter := client.Collection("Users").Where("Username", "==", input.Creator).Documents(ctx)
 		for {
 			doc, err := iter.Next()
@@ -111,27 +121,30 @@ func SetSubscribe(c *gin.Context) {
 			if err != nil {
 				return
 			}
-
+			//add document data to subcriber array
 			doc.DataTo(&subArr)
-
+			//add user to new subscriber array
 			var user = input.UserName
 
 			newsubArr = append(newsubArr, user)
 			newsubArr = append(newsubArr, subArr.UsersThatSubscribed...)
-
+			//update the document field to reflect new array that contains user
 			dc := client.Collection("Users").Doc(doc.Ref.ID)
 			_, err = dc.Update(ctx, []firestore.Update{
 				{Path: "SubscriberList", Value: newsubArr},
 			})
+			//increment subscriber count by 1
 			li := client.Collection("Users").Doc(doc.Ref.ID)
 			_, err = li.Update(ctx, []firestore.Update{
 				{Path: "SubscriberCount", Value: firestore.Increment(1)},
 			})
 		}
 
-	} else {
+	} else { //if user unsubscribes (false)
+		//create subscriber array to retrieve current array and new array for updated user
 		var subArr Subscriber
 		var newsubArr []string
+		//iterate through Firestore to find creator document
 		iter := client.Collection("Users").Where("Username", "==", input.Creator).Documents(ctx)
 		for {
 			doc, err := iter.Next()
@@ -141,11 +154,12 @@ func SetSubscribe(c *gin.Context) {
 			if err != nil {
 				return
 			}
+			//add document data to subcriber array
 
 			doc.DataTo(&subArr)
 
 			var user = input.UserName
-
+			//remove user from subscriber array and create new array
 			newsubArr = append(newsubArr, subArr.UsersThatSubscribed...)
 			for i, v := range newsubArr {
 				if v == user {
@@ -153,11 +167,12 @@ func SetSubscribe(c *gin.Context) {
 					break
 				}
 			}
-
+			//update the document field to reflect new array that doesn't contain user
 			dc := client.Collection("Users").Doc(doc.Ref.ID)
 			_, err = dc.Update(ctx, []firestore.Update{
 				{Path: "SubscriberList", Value: newsubArr},
 			})
+			//decrement subscriber count by 1
 			li := client.Collection("Users").Doc(doc.Ref.ID)
 			_, err = li.Update(ctx, []firestore.Update{
 				{Path: "SubscriberCount", Value: firestore.Increment(-1)},
